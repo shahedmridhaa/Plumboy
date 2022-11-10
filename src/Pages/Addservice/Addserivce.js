3747220fe44b5832cbaf5ef8f4354347191b9705
shahedmridhaa <raselmridha9696@gmail.com>
2022-11-10 20:55:29 +0600

    input updet

--- src/Pages/Addservice/Addserivce.js
+++ src/Pages/Addservice/Addserivce.js
@@ -31,7 +31,7 @@ const Addserivce = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        if (data.acknowledged) {
+        if (data.acknowledged){
           form.reset();
           toast.success("Successfully added service!");
         }
@@ -93,7 +93,7 @@ const Addserivce = () => {
               <div className="datepicker relative form-floating mb-3 xl:w-96">
                 <input
                   type="date"
-                  className=" block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  className=" block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-600 focus:bg-white focus:border-blue-600 focus:outline-none"
                   placeholder="Select a date"
                   required
                   name="date"
