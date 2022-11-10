c9dd1712a0c2fafe664d939eeccce41af87cfde7
shahedmridhaa <raselmridha9696@gmail.com>
2022-11-10 21:48:22 +0600

    final commit

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,41 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Banner.css";
-import bannerman from "../../../assest/man.png";
+// import bannerman from "../../../assest/man.png";
 
 const Banner = () => {
   return (
-    <section className="banner">
-      <div className="container flex flex-col justify-center p-6 mx-auto  lg:flex-row lg:justify-between">
-        <div className="flex flex-col justify-center text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-          <span className="dark:text-indigo-400 text-md">GURRENTED</span>
-
-          <h1 className="text-5xl font-bold my-5">
-            Professional Plumbing Service for Your Home
+    <div className="hero banner">
+      <div className=" bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md text-black">
+          <h1 className="mb-5 text-3xl font-bold">
+           Plumbing and Drain Cleaning Professionals
           </h1>
-          <span className="py-2 text-lg">
-          I've been repairing and installing plumbing systems for nearly 50 years. Let us help you enjoy quality plumbing again!
-          </span>
-          <div className="flex flex-col space-y-4 sm:items-center sm:py-20 sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-            <Link
-              rel="noopener noreferrer"
-              to="/blog"
-              className="flex text-white text-center bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
-            >
-              Learn More
-            </Link>
-            
-          </div>
-        </div>
-        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-full">
-          <img
-            src={bannerman}
-            alt=""
-            className="w-96 object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-full"
-          />
+          <p className="mb-5">
+          Mr. Rooter is dedicated to addressing your plumbing needs as quickly as possible. We know that for both residential and commercial clients, you need to have working plumbing every day.
+          </p>
+          <Link to="/blog">
+          <button className="btn btn-primary">Get Started</button>
+          </Link>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
